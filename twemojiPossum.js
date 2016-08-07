751d3fc1964351931670646d965d084dabaa1243
kamalasaurus <kamalasaurus@gmail.com>
2016-08-08 06:15:35 +0800

    converting to rake task

--- twemojiPossum.js
+++ twemojiPossum.js
@@ -37,10 +37,13 @@
 * for contributing to an open future for the web
 * https://twitter.github.io/twemoji/
 *
-* WHY IS THIS IN JAVASCRIPT:
+* WHY IS THIS IN RUBY:
 *
-* Because it is the lingua franca of the internet!  ES5 for maximum
-* portability, no preprocessors here!
+* Because JSDOM and Cheerio both choked to death trying to load the html
+* for the Full Emoji List!  I'm amazed that a 30 MB html file could
+* completely destroy both of those projects with a Stack Allocation Error!
+* I'm only trying to deal with one open source problem at a time, so I'll
+* leave fixing those projects to the next guy 😅
 *
 * NOTES:
 *
@@ -50,8 +53,15 @@
 * unicode documentations here:
 *
 * http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt
+* http://www.unicode.org/Public/emoji/3.0//emoji-zwj-sequences.txt
 * http://www.unicode.org/Public/emoji/3.0//emoji-sequences.txt
-* https://github.com/twitter/twemoji/tree/gh-pages/svg
+*
+* It's a bit easier to scrape the emoji-list found at:
+* http://unicode.org/emoji/charts/full-emoji-list.html
+* than to parse and munge overinclusive text files 😅
+*
+* Since everything is hosted at unicode.org, I'm assuming it'll be
+* maintained properly ¯\_(ツ)_/¯
 ***************************************************************************/
 
 /***************************************************************************
@@ -72,31 +82,8 @@ var jsdom = require('jsdom');
 * to find where they are used in the script 👍
 ***************************************************************************/
 
-var FULL_EMOJI_LIST = 'http://unicode.org/emoji/charts/full-emoji-list.html';
-
-var UNICODE_URL = 'http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt';
-var ZWJS_URL ='http://www.unicode.org/Public/emoji/3.0//emoji-sequences.txt';
-var SEQUENCES_URL = 'http://www.unicode.org/Public/emoji/3.0//emoji-sequences.txt';
+var UNICODE_URL= 'http://unicode.org/emoji/charts/full-emoji-list.html';
 var TWEMOJI_URL = 'https://github.com/twitter/twemoji/tree/gh-pages/svg';
-
-
-
-/***************************************************************************
-* UTILITIES: utility functions for processing various files
-***************************************************************************/
-
-function convertFileToList(filePath) {
-  return fs.readFileSync(filePath)
-    .toString()
-    .split(/\n/);
-}
-
-function unicodeKeyMapper(map, unicodeEntry) {
-  var relationshipList = unicodeEntry.split(';');
-  var codePoint = relationshipList[0].toLowerCase();
-  var readableName = relationshipList[1].toLowerCase();
-  return obj[codePoint] = readableName;
-}
 
 
 
@@ -105,53 +92,109 @@ function unicodeKeyMapper(map, unicodeEntry) {
 * same name
 ****************************************************************************/
 
-// these are the base unicode entries found at UNICODE_URL:
-// http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt
-function _getUnicode(resolve, reject) {
-  var unicodeSrc = 'http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt';
-  var unicodeOut = path.join(__dirname, 'unicodeList.txt');
+function _getUnicode() {
 
-  var download = wget.download(unicodeSrc, unicodeOut);
-  download.on('end', function(filePath) {
-    var unicodeList = convertFileToList(filePath);
-    var unicodeMap = unicodeList.reduce(unicodeKeyMapper, {});
-    resolve(unicodeMap);
-    return;
-  });
-  download.on('error', function(error) {
-    reject(error);
-    return;
-  });
+  jsdom.env({
+    url: 'http://unicode.org/emoji/charts/full-emoji-list.html',
+    scripts: ['http://code.jquery.com/jquery.js'],
+    done: function(err, window) {
+      if (err) {
+        console.eror(err);
+        return;
+      }
+      console.log('hello');
+      return;
+    }
+  })
+
 }
 
-// these are the semi-standardized Modified sequences found at SEQUENCES_URL:
-// http://www.unicode.org/Public/emoji/3.0//emoji-sequences.txt
-function _getSequences(resolve, reject) {}
+_getUnicode();
 
-// these are the semi-standardized ZWJ sequences found at ZWJS_URL:
-// http://www.unicode.org/Public/emoji/3.0//emoji-zwj-sequences.txt
-function _getZWJs(resolve, reject) {}
+// these are the base unicode entries found at UNICODE_URL:
+// http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt
+//function _getUnicode(resolve, reject) {
+
+  //jsdom.env({
+    //url: (UNICODE_URL),
+    //scripts: ["http://code.jquery.com/jquery.js"],
+    //done: function(errors, window) {
+      //if (errors) {
+        //reject(errors);
+        //return;
+      //}
+
+      //console.log('hello');
+
+
+      //var $ = window.$;
+
+      //var keyMap = {};
+
+      //var unicodeEntries = $('tr').filter(function(i, el) {
+        //// there's a header line every few rows
+        //return $(el).find('th').length === 0;
+      //});
+
+      //unicodeEntries.forEach(function(i, el) {
+        //$el = $(el);
+        //var code = $el.find('td.code').attr('name');
+        //var name = $el.find('td.name')
+          //.text()
+          //.toLowerCase()
+          //.trim()
+          //.replace(/\w/g, ' ')
+          //.split(' ')
+          //.join('-');
+        //console.log(code, name);
+        //keyMap[code] = name;
+      //});
+
+      //resolve(keyMap);
+      //return;
+    //}
+  //});
+
+//}
+
+// these are the base unicode entries found at UNICODE_URL:
+// http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt
+//function _getUnicode(resolve, reject) {
+  //var unicodeSrc = 'http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt';
+  //var unicodeOut = path.join(__dirname, 'unicodeList.txt');
+
+  //var download = wget.download(unicodeSrc, unicodeOut);
+  //download.on('end', function(filePath) {
+    //var unicodeList = convertFileToList(filePath);
+    //var unicodeMap = unicodeList.reduce(unicodeKeyMapper, {});
+    //resolve(unicodeMap);
+    //return;
+  //});
+  //download.on('error', function(error) {
+    //reject(error);
+    //return;
+  //});
+//}
 
 // these are the code-points currently supported by twemoji in svg format found at TWEMOJI_URL:
 // https://github.com/twitter/twemoji/tree/gh-pages/svg
-function _getTwemoji() {
+//function _getTwemoji(resolve, reject) {
 
-  jsdom.env({
-    url: ('https://github.com/twitter/twemoji/tree/gh-pages/svg'),
-    scripts: ["http://code.jquery.com/jquery.js"],
-    done: function(errors, window) {
-      if (errors) { console.error(errors); }
+  //jsdom.env({
+    //url: ('https://github.com/twitter/twemoji/tree/gh-pages/svg'),
+    //scripts: ["http://code.jquery.com/jquery.js"],
+    //done: function(errors, window) {
+      //if (errors) { console.error(errors); }
 
-      var $ = window.$;
-      var txt = $('pre').text();
+      //var $ = window.$;
+      //var txt = $('pre').text();
 
-      var codePoints = $('td.content a').toArray()
+      //var codePoints = $('td.content a').toArray()
 
-      writeFile(txt);
-    }
-  });
+    //}
+  //});
 
-}
+//}
 
 
 
@@ -161,11 +204,15 @@ function _getTwemoji() {
 ***************************************************************************/
 
 // a key map of all base unicode sequences listedby the unicode consortium
-var getUnicode = new Promise(_getUnicode);
-var getSequences = new Promise(_getSequences);
-var getZWJs = new Promise(_getZWJs);
-var getTwemoji = new Promise(_getTwemoji);
+//var getUnicode = new Promise(_getUnicode);
+//var getTwemoji = new Promise(_getTwemoji);
 
+//Promise.all([getUnicode]).then(function(values) {
+  //console.log(values);
+  //console.log('end');
+//}, function(errors) {
+  //console.error(errors);
+//});
 
 
 //Promise.all([getUnicode, getModifieds, getZWJs, getTwemojis]).then(function(values) {
